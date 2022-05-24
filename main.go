d29662fcb948d74cdda0d39e0228ea6d12b54e0f
panda1835 <phuc.le@ug.fuv.edu.vn>
2022-05-24 22:18:10 +0700

    give each new crop image in bucket a different filename

--- main.go
+++ main.go
@@ -1,68 +1,12 @@
 package main
 
 import (
-	"fmt"
-	_ "image/jpeg"
-	_ "image/png"
-	"log"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
-	"github.com/panda1835/go-api/api"
-	"github.com/panda1835/go-api/image_processing"
+	"github.com/panda1835/go-api/restapi"
 )
 
 func main() {
 	router := gin.Default()
-	router.POST("/upload", postImage)
+	router.POST("/upload", restapi.PostImage)
 	router.Run("localhost:8080")
-}
-
-type post_response struct {
-	Name  string  `json:"name"`
-	Score float32 `json:"score"`
-	URL   string  `json:"crop_url"`
-}
-
-func postImage(c *gin.Context) {
-	img_dest := "upload_image/recent_image"
-
-	// fetch image
-	file, _ := c.FormFile("file")
-
-	// save image
-	c.SaveUploadedFile(file, img_dest)
-
-	var predictions, err = api.DetectLabels(img_dest)
-
-	var response []post_response
-	for i := 0; i < len(predictions); i++ {
-		response = append(response, post_response{})
-		// assign prediction name and score
-		response[i].Name = predictions[i].Name
-		response[i].Score = predictions[i].Score
-
-		// crop image
-		crop_file_name := fmt.Sprintf("crop_image/recent_crop_image_%d", i)
-		err = image_processing.CropBoundingBox(img_dest, crop_file_name, predictions[i].Coords)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// upload to google cloud bucket
-		img_url, err := api.UploadFile("go-api", crop_file_name)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		response[i].URL = img_url
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		c.IndentedJSON(http.StatusAccepted, response)
-	}
-
-	fmt.Println("done")
 }
